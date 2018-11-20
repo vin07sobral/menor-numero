e98e54b7306712c3375451a41f437278da375633
Marcus Vinicius Sobral <mvinicius.sobral@gmail.com>
2018-11-20 10:41:53 -0200

    Add files via upload

---
+++ menor numero/menor numero/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace menor_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X, Y, Z, resultado;
+
+            X = int.Parse(Console.ReadLine());
+            Y = int.Parse(Console.ReadLine());
+            Z = int.Parse(Console.ReadLine());
+
+            resultado = util.menorNumero(X, Y, Z);
+
+            Console.WriteLine("Menor =" + resultado);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ menor numero/menor numero/util.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace menor_numero
+{
+    class util
+    {
+
+        public static int menorNumero( int a, int b, int c)
+        {
+            if (a<b && a < c)
+            {
+                return a;
+            }
+            else if (b < c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+    }
+}
